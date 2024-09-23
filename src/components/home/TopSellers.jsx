dcff3015138423085fd4e8069d858c6efcbe41bf
Zeeshan <zeeshan.hu@outlook.com>
2024-09-23 14:53:47 -0400

    TopSellers completed

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers")
+      .then((response) => {
+        setTopSellers(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching top sellers:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <section id="section-popular" className="pb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="text-center">
+                <h2>Top Sellers</h2>
+                <div className="small-border bg-color-2"></div>
+              </div>
+            </div>
+            <div className="col-md-12">
+              <ol className="author_list">
+                {new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                  <div className="author_list_pp">
+                    <Skeleton width="50px" height="50px" borderRadius="50%" />
+                  </div>
+                  <div className="author_list_info">
+                    <div>
+                      <Skeleton width="100px" height="20px" borderRadius="4px" />
+                    </div>
+                    <div>
+                      <Skeleton width="50px" height="20px" borderRadius="4px" />
+                    </div>
+                  </div>
+                </li>                
+                ))}
+              </ol>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,21 +68,23 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
+              {topSellers.map((seller, index) => (
                 <li key={index}>
                   <div className="author_list_pp">
-                    <Link to="/author">
+                    <Link to={`/author/${seller.authorId}`}>
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
+                        src={seller.authorImage}
+                        alt={seller.authorName}
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to={`/author/${seller.authorId}`}>
+                      {seller.authorName}
+                    </Link>
+                    <span>{seller.price} ETH</span>
                   </div>
                 </li>
               ))}
