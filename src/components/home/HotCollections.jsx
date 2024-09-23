998f87f53513ce78b7f32021b23a4e376ed19c47
Zeeshan <zeeshan.hu@outlook.com>
2024-09-23 11:24:57 -0400

    Author Link fix

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -125,11 +125,11 @@ const HotCollections = () => {
                       </Link>
                     </div>
                     <div className="nft_coll_pp">
-                      <Link to="/author">
+                      <Link to={`/author/${collection.authorId}`}>
                         <img
                           className="lazy pp-coll"
                           src={collection.authorImage}
-                          alt=""
+                          alt="author"
                         />
                       </Link>
                       <i className="fa fa-check"></i>
